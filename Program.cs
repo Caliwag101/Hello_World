d9b3dacde81c8723f49fc8c0d533c71d4c1d9f0c
Callum Fraser <frasercallum500@gmail.com>
2021-09-14 11:02:44 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            for (x = 1; x < 6; x++)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press the enter key to close this :)");
+            Console.WriteLine("idk what else to put here");
+            Console.ReadLine();
+        }
+    }
+}
